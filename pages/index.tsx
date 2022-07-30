9674903499990aef0e9a541aa25a3439db1bb592
PiotrSierant <piotr.sierant96@gmail.com>
2022-07-30 11:08:51 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
-import { AppShell, Center, Container, Header, Title } from '@mantine/core';
+import { AppShell, Center, Container, Header, Space, Title } from '@mantine/core';
+import { Post as PostPrisma } from '@prisma/client';
 import type { NextPage } from 'next';
+import { Post } from '../components/post/Post';
 
-const Home: NextPage = () => {
+import { prisma } from '../lib/prisma';
+
+export const getServerSideProps = async () => {
+  const posts = await prisma.post.findMany({ orderBy: { createdAt: 'desc' } });
+  return { props: { posts: JSON.parse(JSON.stringify(posts)) } };
+};
+
+type Props = {
+  posts: PostPrisma[];
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <AppShell
       padding="lg"
@@ -14,8 +27,13 @@ const Home: NextPage = () => {
         main: { height: '100%' },
       })}
     >
-      <Container>
-        <Center> Tu będą posty </Center>
+      <Container size="lg">
+        {posts.map((post) => (
+          <>
+            <Post title={post.title} date={post.createdAt} content={post.content} key={post.id} slug={post.slug} />
+            <Space h="md" />
+          </>
+        ))}
       </Container>
     </AppShell>
   );
